18ac200929dcfad5ee5bf3d90335da849caee10a
Bagas Maulana <bagas.maulanarela@gmail.com>
2025-12-25 11:44:24 +0700

    aaaAAQ

--- components/appointment/charts/stat-summary.tsx
+++ components/appointment/charts/stat-summary.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { ResponsiveContainer, RadialBarChart, RadialBar } from "recharts";
 import { Users } from "lucide-react";
 import { formatNumber } from "@/utils";
